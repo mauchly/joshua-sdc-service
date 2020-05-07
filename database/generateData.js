c4bf342c17ab7fcb57f0a602c7f200af2fd040c6
Joshua Eisenmann <thisiseisenmann@gmail.com>
2020-05-06 22:22:44 -0700

    Refactor data generation function, added comments

--- database/generateData.js
+++ database/generateData.js
@@ -1,14 +1,17 @@
 const loremIpsum = require("lorem-ipsum").loremIpsum;
 const fs = require('fs');
 
+//Needed to allow multiple loops to run without timeout
 require('events').EventEmitter.defaultMaxListeners = 50;
 
 const listing_type_options = ['Entire place', 'Private room', 'Hotel room', 'Shared room'];
 const listing_category_options = ['apartment','house', 'hotel', 'cabin', 'bnb'];
 
+//Streams to process both files
 let listingsStream = fs.createWriteStream('./database/listingsCSV');
 let listing_imagesStream = fs.createWriteStream('./database/listings_imagesCSV');
 
+//Initial row for each table
 const listingDataLabels = `listing_id, listing_type listing_category, night_price, avg_review, num_review, number_beds, listing_title, is_fav\n`;
 const listingImagesDataLabels = `listing_id, image_url\n`;
 
@@ -49,60 +52,32 @@ const generateDataListingsImagesTable = (id) => {
   return result;
 }
 
-const createListingsCSV = () => {
+// //Method called to generate 10M records, drain method need to clear out buffer to allow multiple writes
+const recordGenerator = (dataFunc, stream, initString) => {
   let listingId = 1;
   for (let i = 0; i < 1000; i++) {
     if (i === 0) {
-      let result = generateDataListingsTable(listingId);
-      listingsStream.write(listingDataLabels);
-      let write = listingsStream.write(result);
+      let result = dataFunc(listingId);
+      stream.write(initString);
+      let write = stream.write(result);
       if (!write) {
-        listingsStream.once('drain', createListingsCSV);
-      } else {
-        createListingsCSV();
+        stream.once('drain', dataFunc);
       }
     } else {
       listingId += 10000;
-      let data = generateDataListingsTable(listingId);
-      let write = listingsStream.write(data);
+      let data = dataFunc(listingId);
+      let write = stream.write(data);
       if (!write) {
-        listingsStream.once('drain', createListingsCSV);
-      } else {
-        createListingsCSV();
+        stream.once('drain', dataFunc);
       }
     }
     console.log('listingId', listingId);
   }
-  listingsStream.end();
+  stream.end();
 }
 
+// createListingsCSV();
+// createListingsImagesCSV();
 
-const createListingsImagesCSV = () => {
-  let listingId = 1;
-  for (let i = 0; i < 1000; i++) {
-    if (i === 0) {
-      let result = generateDataListingsImagesTable(listingId);
-      listing_imagesStream.write(listingImagesDataLabels);
-      let write = listing_imagesStream.write(result);
-      if (!write) {
-        listing_imagesStream.once('drain', createListingsImagesCSV);
-      } else {
-        createListingsImagesCSV();
-      }
-    } else {
-      listingId += 10000;
-      let data = generateDataListingsImagesTable(listingId);
-      let write = listing_imagesStream.write(data);
-      if (!write) {
-        listing_imagesStream.once('drain', createListingsImagesCSV);
-      } else {
-        createListingsImagesCSV();
-      }
-    }
-    console.log('listingId', listingId);
-  }
-  listing_imagesStream.end();
-}
-
-createListingsCSV();
-createListingsImagesCSV();
+recordGenerator(generateDataListingsTable, listingsStream, listingDataLabels);
+recordGenerator(generateDataListingsImagesTable, listing_imagesStream, listingImagesDataLabels);
