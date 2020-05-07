4763a12650e75fc5c020ef542d6041200ad6e614
Joshua Eisenmann <thisiseisenmann@gmail.com>
2020-05-06 20:17:14 -0700

    Generating required records for both tables

--- database/generateData.js
+++ database/generateData.js
@@ -1,20 +1,22 @@
 const loremIpsum = require("lorem-ipsum").loremIpsum;
 const fs = require('fs');
 
+require('events').EventEmitter.defaultMaxListeners = 50;
+
 const listing_type_options = ['Entire place', 'Private room', 'Hotel room', 'Shared room'];
 const listing_category_options = ['apartment','house', 'hotel', 'cabin', 'bnb'];
 
 let listingsStream = fs.createWriteStream('./database/listingsCSV');
 let listing_imagesStream = fs.createWriteStream('./database/listings_imagesCSV');
 
-const listingDataLabels = `listing_id, listing_type listing_category, night_price, avg_review, num_review, number_beds, listing_title, is_fav`;
-const listingImagesDataLabels = `listing_id, image_url`;
+const listingDataLabels = `listing_id, listing_type listing_category, night_price, avg_review, num_review, number_beds, listing_title, is_fav\n`;
+const listingImagesDataLabels = `listing_id, image_url\n`;
 
 //generate data for listings table
-let generateDataListingsTable = () => {
-  let result = '';
+const generateDataListingsTable = (id) => {
+  let listingsResult = '';
   for (let i = 0; i < 10000; i ++) {
-    let listing_id = 10001 + i;
+    let listing_id = id + i;
     let listing_type =  listing_type_options[Math.floor(Math.random() * 4)];
     let listing_category = listing_category_options[Math.floor(Math.random() * 5)];
     let night_price = 100 + Math.random() * 100;
@@ -29,18 +31,18 @@ let generateDataListingsTable = () => {
       units: "sentences"
     });
     let is_fav = i % 20 === 0 ? true : false;
-    result += `${listing_id}, ${listing_type} ${listing_category}, ${night_price}, ${avg_review}, ${num_review}, ${number_beds}, ${listing_title}, ${is_fav}`
+    listingsResult += `${listing_id}, ${listing_type} ${listing_category}, ${night_price}, ${avg_review}, ${num_review}, ${number_beds}, ${listing_title}, ${is_fav}\n`;
   }
-  return result;
+  return listingsResult;
 }
 
 //generate data for listing_images
-let generateDataListingsImagesTable = () => {
+const generateDataListingsImagesTable = (id) => {
   let result = '';
   for (let i = 0; i < 10000; i ++) {
-    let listing_id = 10001 + i;
+    let listing_id = id + i;
     for (let j = 0; j < 5; j++) {
-      let image_url = `https://picsum.photos/300/200?random=${listing_id}${j}`;
+      let image_url = `https://picsum.photos/300/200?random=${listing_id}${j}\n`;
       result += `${listing_id}, ${image_url}`;
     }
   }
@@ -48,17 +50,57 @@ let generateDataListingsImagesTable = () => {
 }
 
 const createListingsCSV = () => {
-  let result = generateDataListingsTable();
-  listingsStream.write(listingDataLabels);
-  listingsStream.write(result);
+  let listingId = 1;
+  for (let i = 0; i < 1000; i++) {
+    if (i === 0) {
+      let result = generateDataListingsTable(listingId);
+      listingsStream.write(listingDataLabels);
+      let write = listingsStream.write(result);
+      if (!write) {
+        listingsStream.once('drain', createListingsCSV);
+      } else {
+        createListingsCSV();
+      }
+    } else {
+      listingId += 10000;
+      let data = generateDataListingsTable(listingId);
+      let write = listingsStream.write(data);
+      if (!write) {
+        listingsStream.once('drain', createListingsCSV);
+      } else {
+        createListingsCSV();
+      }
+    }
+    console.log('listingId', listingId);
+  }
   listingsStream.end();
 }
 
 
 const createListingsImagesCSV = () => {
-  let result = generateDataListingsImagesTable();
-  listing_imagesStream.write(listingImagesDataLabels);
-  listing_imagesStream.write(result);
+  let listingId = 1;
+  for (let i = 0; i < 1000; i++) {
+    if (i === 0) {
+      let result = generateDataListingsImagesTable(listingId);
+      listing_imagesStream.write(listingImagesDataLabels);
+      let write = listing_imagesStream.write(result);
+      if (!write) {
+        listing_imagesStream.once('drain', createListingsImagesCSV);
+      } else {
+        createListingsImagesCSV();
+      }
+    } else {
+      listingId += 10000;
+      let data = generateDataListingsImagesTable(listingId);
+      let write = listing_imagesStream.write(data);
+      if (!write) {
+        listing_imagesStream.once('drain', createListingsImagesCSV);
+      } else {
+        createListingsImagesCSV();
+      }
+    }
+    console.log('listingId', listingId);
+  }
   listing_imagesStream.end();
 }
 
